03ed6c6f7017e022f282843606b523c3d17d39e7
kutta75 <kutta75@gmail.com>
2022-02-25 13:32:26 +0100

    ajout app studio

--- espanol/urls.py
+++ espanol/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('keyboard/',include('keyboard.urls')),
+    path('studio/',include('studio.urls')),
     path('verbos/',include('verbos.urls')),
         ]
